1429b906dfc0fb9edcd854cb0df5b7c85994066a
LSaldyt <lucassaldyt@gmail.com>
2017-02-13 20:53:56 -0700

    Modifies map example

--- map-color.v
+++ map-color.v
@@ -1,31 +1,38 @@
-// Say whether a given four-color map coloring is valid.
-// The map corresponds to the regions in the Land of Oz:
+// Slightly more complicated map-coloring example
 //
-//                     [Gillikin Country]
-//                    /         |        \
-// [Munchkin Country] -- [Emerald City] -- [Winkie Country]
-//                    \         |        /
-//                     [Quadling Country]
-//
-// Author: Scott Pakin <pakin@lanl.gov>
+// [a] - [b] - [c]
+//  |     |  /  | 
+// [d] - [e] - [f]
+//  |  /  |     |
+// [g] - [h] - [i]
 
-module map_color (GC, WC, QC, MC, EC, valid);
-   input [1:0] GC;
-   input [1:0] WC;
-   input [1:0] QC;
-   input [1:0] MC;
-   input [1:0] EC;
-   output      valid;
-   wire [7:0]  tests;
+module map_color(A, B, C, D, E, F, G, H, I, valid);
+    input [1:0] A;
+    input [1:0] B;
+    input [1:0] C;
+    input [1:0] D;
+    input [1:0] E;
+    input [1:0] F;
+    input [1:0] G;
+    input [1:0] H;
+    input [1:0] I;
+    output valid;
 
-   assign tests[0] = GC != WC;
-   assign tests[1] = WC != QC;
-   assign tests[2] = QC != MC;
-   assign tests[3] = MC != GC;
-   assign tests[4] = EC != GC;
-   assign tests[5] = EC != WC;
-   assign tests[6] = EC != QC;
-   assign tests[7] = EC != MC;
+    wire [12:0]  tests;
 
-   assign valid = &tests[7:0];
+    assign tests[0] = A != B;
+    assign tests[1] = A != D;
+    assign tests[2] = B != C;
+    assign tests[3] = B != E;
+    assign tests[4] = C != E;
+    assign tests[5] = D != E;
+    assign tests[6] = D != G;
+    assign tests[7] = E != G;
+    assign tests[8] = E != H;
+    assign tests[9] = E != F;
+    assign tests[10] = F != I;
+    assign tests[11] = G != H;
+    assign tests[12] = H != I;
+
+    assign valid = &tests[12:0];
 endmodule // map_color
