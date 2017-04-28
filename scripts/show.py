aa35d1675235e2b00a04f5bd7d4fa0031dd84ade
LSaldyt <lucassaldyt@gmail.com>
2017-04-28 12:39:25 -0600

    Cleans up repository

--- scripts/show.py
+++ scripts/show.py
@@ -4,14 +4,15 @@ import subprocess, shutil, sys, os
 def gen_image(scriptname, filename, fid='', view=True):
     basename = os.path.splitext(filename)[0]
     subprocess.call('yosys %s scripts/%s' % (filename, scriptname), shell=True)
-    #shutil.copy('/home/lucas/.yosys_show.png', 
-    #            'circuitimages/%s.png' % (basename + fid))
-    #subprocess.call('xdg-open circuitimages/%s.png' % (basename + fid), shell=True)
+    shutil.copy('/home/lucas/.yosys_show.png', 
+                'circuitimages/%s.png' % (basename + fid))
+    if view:
+        subprocess.call('xdg-open circuitimages/%s.png' % (basename + fid), shell=True)
 
 def show(files):
     for filename in files:
         gen_image('show.ys', filename, view=True)
-        #gen_image('optshow.ys', filename, '_opt', view=True)
+        gen_image('optshow.ys', filename, '_opt', view=True)
 
 if __name__ == "__main__":
     show(sys.argv[1:])
