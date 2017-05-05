fef2863798677e066c822d81a494afe54fe38d77
LSaldyt <lucassaldyt@gmail.com>
2017-05-05 09:32:51 -0600

    Adds reading of bool sat problems

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,7 +1,8 @@
-from .run      import run
-from .create   import create_knapsack, create_map_color
-from .show     import show
-from .rank     import rank 
-from .solve    import solve
-from .verify   import verify
-from .run_qasm import run_qasm
+from .run          import run
+from .create       import create_knapsack, create_map_color
+from .show         import show
+from .rank         import rank 
+from .solve        import solve
+from .verify       import verify
+from .run_qasm     import run_qasm
+from .read_verilog import read_verilog
