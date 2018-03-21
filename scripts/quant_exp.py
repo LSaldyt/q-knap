750a32d2240e100cd5e7b48af4f4e03a824ee9fe
LSaldyt <lucassaldyt@gmail.com>
2018-03-20 21:40:19 -0700

    Removes figures from paper and adds optional imports

--- scripts/quant_exp.py
+++ scripts/quant_exp.py
@@ -1,23 +1,32 @@
-from IBMQuantumExperience import IBMQuantumExperience
+try:
+    from IBMQuantumExperience import IBMQuantumExperience
 
-from pprint import pprint
+    from pprint import pprint
 
-def quant_exp(args):
-    with open('etc/IBMtoken', 'r') as infile:
-        token = infile.read().replace('\n', '')
-    config = {
-       "url": 'https://quantumexperience.ng.bluemix.net/api'
-    }
+    def quant_exp(args):
+        with open('etc/IBMtoken', 'r') as infile:
+            token = infile.read().replace('\n', '')
+        config = {
+           "url": 'https://quantumexperience.ng.bluemix.net/api'
+        }
 
-    QuantExp = IBMQuantumExperience.IBMQuantumExperience
-    api = QuantExp(token, config)
-    for code in api.get_last_codes():
-        pprint(code)
-        '''
-        name = code['name']
-        if 'grover' in name.lower():
-            print('IBM Results for{}'.format(name))
-            pprint(code['executions'][-1]['result'])
-            #pprint(code.keys())
-            #pprint(api.get_execution(name))
-        '''
+        QuantExp = IBMQuantumExperience.IBMQuantumExperience
+        api = QuantExp(token, config)
+        for code in api.get_last_codes():
+            pprint(code)
+            '''
+            name = code['name']
+            if 'grover' in name.lower():
+                print('IBM Results for{}'.format(name))
+                pprint(code['executions'][-1]['result'])
+                #pprint(code.keys())
+                #pprint(api.get_execution(name))
+            '''
+except ModuleNotFoundError:
+    print('IBM suite not installed')
+    #1/0
+
+    def quant_exp(args):
+        1/0
+
+
--- scripts/run_qasm.py
+++ scripts/run_qasm.py
@@ -1,24 +1,28 @@
-from QuantumComputer import *
+try:
+    from QuantumComputer import *
 
-def clean(line):
-    if '//' in line:
-        line, comment = line.split('//', 1)
-    return line
+    def clean(line):
+        if '//' in line:
+            line, comment = line.split('//', 1)
+        return line
 
-def run_qasm(args):
-    assert len(args) >= 1
-    filename = args[0]
-    if len(args) == 1:
-        containing = ['q0', 'q1', 'q2', 'q3', 'q4']
-    else:
-        containing = args[1:]
-    with open(filename, 'r') as qasmfile:
-        content = [clean(line) for line in qasmfile]
-    code = '\n'.join(content)
-    print(code)
-    qc = QuantumComputer()
-    qc.reset()
-    qc.execute(code)
-    for c in containing:
-        Probability.pretty_print_probabilities(qc.qubits.get_quantum_register_containing(c).get_state())
+    def run_qasm(args):
+        assert len(args) >= 1
+        filename = args[0]
+        if len(args) == 1:
+            containing = ['q0', 'q1', 'q2', 'q3', 'q4']
+        else:
+            containing = args[1:]
+        with open(filename, 'r') as qasmfile:
+            content = [clean(line) for line in qasmfile]
+        code = '\n'.join(content)
+        print(code)
+        qc = QuantumComputer()
+        qc.reset()
+        qc.execute(code)
+        for c in containing:
+            Probability.pretty_print_probabilities(qc.qubits.get_quantum_register_containing(c).get_state())
 
+except ModuleNotFoundError:
+    print('QuantumComputer module not installed')
+    #1/0
